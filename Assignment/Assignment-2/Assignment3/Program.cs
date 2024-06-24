f016d091afed81de068e9b0135092a533df30fd9
saivardhan-fini <saivardhanm@infinite.com>
2024-06-24 10:24:30 +0530

    Add files via upload

---
+++ Assignment/Assignment-2/Assignment3/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class Accounts
+    {
+        public int accountNo;
+        public string customerName;
+        public string accountType;
+        public string transactionType;
+        public int amount;
+        public int balance;
+
+        public Accounts(int accountNo, string customerName, string accountType, string transactionType, int amount, int balance)
+        {
+            this.accountNo = accountNo;
+            this.customerName = customerName;
+            this.accountType = accountType;
+            this.transactionType = transactionType;
+            this.amount = amount;
+            this.balance = balance;
+        }
+
+        public void UpdateBalance(string transactionType, int amount)
+        {
+            this.transactionType = transactionType;
+            this.amount = amount;
+            if (transactionType.Equals("d", StringComparison.OrdinalIgnoreCase))
+            {
+                Credit(amount);
+            }
+            else if (transactionType.Equals("w", StringComparison.OrdinalIgnoreCase))
+            {
+                Debit(amount);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Transaction Type");
+            }
+        }
+
+        public void Credit(int amount)
+        {
+            balance += amount;
+        }
+
+        public void Debit(int amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Account No: " + accountNo);
+            Console.WriteLine("Customer Name: " + customerName);
+            Console.WriteLine("Account Type: " + accountType);
+            Console.WriteLine("Transaction Type: " + transactionType);
+            Console.WriteLine("Amount: " + amount);
+            Console.WriteLine("Balance: " + balance);
+        }
+
+        public static void Main()
+        {
+            Accounts account = new Accounts(1035473, "SAI", "Savings", "d", 0, 45000);
+            Console.WriteLine("Initial account data:");
+            account.Show();
+
+            Console.WriteLine("\nPerforming a deposit of 10000.....");
+            account.UpdateBalance("d", 10000);
+            account.Show();
+
+            Console.WriteLine("\nPerforming a withdrawal of 2000....");
+            account.UpdateBalance("w", 2000);
+            account.Show();
+
+            Console.WriteLine("\nAttempting an invalid transaction type...");
+            account.UpdateBalance("x", 10000);
+            account.Show();
+
+            Console.WriteLine("\nAttempting a withdrawal of 15000...");
+            account.UpdateBalance("w", 15000);
+            account.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
