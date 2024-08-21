756c65a76f851d2e504ae1c2b16030dd945e0d5f
saivardhan-fini <saivardhanm@infinite.com>
2024-08-21 11:05:40 +0530

    Add files via upload

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Assignment_1.Models;
+
+namespace Assignment_1.Controllers
+{
+    public class CountryController : ApiController
+    {
+        private static List<Country> countrylist = new List<Country>
+        {
+            new Country { Id = 1, CountryName = "INDIA", Capital = "NEW DELHI" },
+            new Country { Id = 2, CountryName = "USA", Capital = "WASHINGTON DC" },
+            new Country { Id = 3, CountryName = "BANGLADESH", Capital = "DHAKA" },
+            new Country { Id = 4, CountryName = "AFGHANISTAN", Capital = "KABUL" }
+        };
+
+        //To read/view country
+
+        [HttpGet]
+        public IEnumerable<Country> Get()
+        {
+            return countrylist;
+        }
+
+        //To add new country
+        [HttpPost]
+        public List<Country> Post([FromBody] Country cont)
+        {
+            countrylist.Add(cont);
+            return countrylist;
+        }
+
+        //to update the country
+        [HttpPut]
+        public void Put(int contid, [FromUri] Country country)
+        {
+            countrylist[contid - 1] = country;
+        }
+
+        //To delete the country
+        [HttpDelete]
+        public void Delete(int contid)
+        {
+            countrylist.RemoveAt(contid - 1);
+        }
+
+    }
+}
